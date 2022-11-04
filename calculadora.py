d7cfffc47fc0c72298678a383f3f34e4d5c37d33
Marco Rodriguez Melgarejo <mrodrguezmelgarejo@frba.utn.edu.ar>
2022-11-04 17:33:14 -0300

    menu principal 2

--- calculadora.py
+++ calculadora.py
@@ -1,1 +1,26 @@
-asd
+def menu_principal():
+    try:
+        operacion = int(input("Ingrese el numero de operacion"))
+        valor1 = int(input("Ingrese el primer valor"))
+        valor2 = int(input("Ingrese el segundo valor"))
+    except ValueError:
+        print("No se ingreso un numero")
+        return
+
+    match operacion:
+        case 1:
+            resultado = sumar(valor1, valor2)
+        case 2:
+            resultado = restar(valor1, valor2)
+        case 3:
+            resultado = multiplicar(valor1, valor2)
+        case 4: 
+            resultado = dividir(valor1, valor2)
+        case 5:
+            resultado = potencia(valor1, valor2)
+        case 6:
+            resultado = raiz(valor1, valor2)
+        case _:
+            resultado = "Operacion invalida"
+
+    print(resultado)
